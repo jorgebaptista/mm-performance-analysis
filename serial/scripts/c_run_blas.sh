86bf2454494ed5400201dc2ff36c28c0fcb6c2db
Jorge Baptista <jorgebaptista.dev@outlook.com>
2024-12-19 19:39:51 +0000

    Add BLAS-based serial matrix multiplication and SLURM script

---
+++ serial/scripts/c_run_blas.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------	#
+#SBATCH -p hpc
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --time=02:30:00
+#SBATCH --qos=cpuvlabualg
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=a90113@ualg.pt
+#SBATCH --job-name=blas-serial-analysis
+#SBATCH --output=../logs/cirrus/%j/%x_%j.out
+#SBATCH --error=../logs/cirrus/%j/%x_%j.err
+
+# ---------------------------------------------------------------------	#
+echo
+echo "=== Environment ==="
+module purge
+module load gcc-13.2
+module load gcc11/libs/openblas/0.3.21
+
+# *******Variables***********
+MACHINE=$(hostname)
+SESSION_DESCRIPTION="serial multiplication"
+TOTAL_MEM_ALLOC=$((SLURM_JOB_NUM_NODES * SLURM_CPUS_ON_NODE * SLURM_MEM_PER_CPU))
+
+MATRIX_TYPE=${1:-float}
+MIN_P=${2:-1}
+MAX_P=${3:-12}
+
+# ***************************
+BIN_DIR="../bin"
+DATA_DIR="../../data"
+LOGS_DIR="../logs/cirrus/$SLURM_JOB_ID"
+RESULTS_DIR="$LOGS_DIR/results"
+
+GENERATE_MATRIX_SOURCE="../../src/generate_matrix.c"
+GENERATE_MATRIX_EXE="$BIN_DIR/generate_matrix_$SLURM_JOB_ID"
+MULTIPLY_MATRIX_SOURCE="../src/multiply_matrix.c"
+LOG_TIMES="$LOGS_DIR/times.log"
+RAND_DATA="random_${MATRIX_TYPE}_matrix_${MAX_P}.bin"
+DIAG_DATA="diag_${MATRIX_TYPE}_matrix_${MAX_P}.bin"
+
+mkdir -p "$BIN_DIR" "$DATA_DIR" "$LOGS_DIR" "$RESULTS_DIR"
+
+# *****Generate Matrices******
+if ([[ ! -f "$DATA_DIR/$DIAG_DATA" ]] || [[ ! -f "$DATA_DIR/$RAND_DATA" ]]) || ([[ " $@ " =~ " -n " ]]); then
+    echo "=== Compiling $GENERATE_MATRIX_SOURCE ==="
+    gcc -Wall -O3 -o "$GENERATE_MATRIX_EXE" "$GENERATE_MATRIX_SOURCE" -DSIZE=$((2 ** $MAX_P)) -DMATRIX_TYPE=$MATRIX_TYPE
+    if [ $? -ne 0 ]; then
+        echo "Compilation failed."
+        exit 1
+    fi
+    echo "Compilation succeeded."
+
+    echo "=== Running $GENERATE_MATRIX_EXE ==="
+    chmod u+x "$GENERATE_MATRIX_EXE"
+    ./"$GENERATE_MATRIX_EXE" "$DATA_DIR/$RAND_DATA" "$DATA_DIR/$DIAG_DATA"
+    if [ $? -ne 0 ]; then
+        echo "Execution failed."
+        rm -f "$GENERATE_MATRIX_EXE"
+        exit 1
+    fi
+    echo "Execution succeeded."
+
+    rm -f "$GENERATE_MATRIX_EXE"
+
+else
+    echo "Using existing input data..."
+fi
+
+# *****Multiply Matrices******
+run_matrix_multiplication() {
+    local input_file=$1
+    local description=$2
+
+    echo "===============================================" | tee -a "$LOG_TIMES"
+    echo "Using $description input file:" >>"$LOG_TIMES"
+
+    for ((power = MIN_P; power <= MAX_P; power++)); do
+        size=$((2 ** power))
+
+        MULTIPLY_MATRIX_EXE="$BIN_DIR/multiply_matrix_${size}x${size}_$SLURM_JOB_ID"
+        LOG_RESULTS="$RESULTS_DIR/${size}x${size}_results.log"
+        echo "Matrix product from $description:" >>"$LOG_RESULTS"
+        echo "------------${size}x${size}-------------" | tee -a "$LOG_TIMES"
+
+        echo "Compiling multiply_matrix.c"
+        rm -f "$MULTIPLY_MATRIX_EXE"
+        gcc -Wall -o "$MULTIPLY_MATRIX_EXE" "$MULTIPLY_MATRIX_SOURCE" -DSIZE=$size -DMAX_SIZE=$((2 ** $MAX_P)) -DMATRIX_TYPE=$MATRIX_TYPE -lopenblas
+        if [ $? -ne 0 ]; then
+            echo "Compilation failed."
+            break
+        fi
+        echo "Compilation succeeded."
+
+        echo "Running $MULTIPLY_MATRIX_EXE"
+        chmod u+x "$MULTIPLY_MATRIX_EXE"
+        /usr/bin/time -v -o "$LOG_TIMES" -a ./"$MULTIPLY_MATRIX_EXE" "$input_file" "$LOG_TIMES" "$LOG_RESULTS"
+        if [ $? -ne 0 ]; then
+            echo "Execution failed."
+            rm -f "$MULTIPLY_MATRIX_EXE"
+            break
+        fi
+
+        echo "Execution succeeded."
+        rm -f "$MULTIPLY_MATRIX_EXE"
+
+        echo "===============================================" >>"$LOG_RESULTS"
+        echo "Times saved to $LOG_TIMES"
+        echo "Results saved to $LOG_RESULTS"
+    done
+}
+
+# *******Start Session******** #
+start_time=$(date +%s)
+echo "Session started at: $(date '+%Y-%m-%d %H:%M:%S')" | tee -a "$LOG_TIMES"
+echo "Running $SESSION_DESCRIPTION with $MATRIX_TYPE values on $MACHINE" | tee -a "$LOG_TIMES"
+# Run multiply_matrix.c with different args
+run_matrix_multiplication "$DATA_DIR/$RAND_DATA" "$RAND_DATA"
+run_matrix_multiplication "$DATA_DIR/$DIAG_DATA" "$DIAG_DATA"
+end_time=$(date +%s)
+
+# *******Finish Session******* #
+echo
+echo "=== Finalizing ==="
+elapsed_seconds=$((end_time - start_time))
+elapsed_minutes=$((elapsed_seconds / 60))
+remaining_seconds=$((elapsed_seconds % 60))
+echo "===============================================" >>"$LOG_TIMES"
+echo "Session Time: ${elapsed_minutes}m ${remaining_seconds}s" | tee -a "$LOG_TIMES"
+echo "Session Completed Successfully at: $(date '+%Y-%m-%d %H:%M:%S')" | tee -a "$LOG_TIMES"
+echo "###############################################" >>"$LOG_TIMES"
+
+# *********Log Session******** #
+echo "Details for Job ID $SLURM_JOB_ID" >>"$LOG_TIMES"
+echo "Running $SESSION_DESCRIPTION with $MATRIX_TYPE values on $MACHINE" >>"$LOG_TIMES"
+echo "Allocated Memory (total): ${TOTAL_MEM_ALLOC} MB" >>"$LOG_TIMES"
+echo "=============== Active Modules =================" >>"$LOG_TIMES"
+module list 2>&1 | awk NF | sed 's/^/ - /' >>"$LOG_TIMES"
+echo "================ Memory Usage ==================" >>"$LOG_TIMES"
+sstat -j "${SLURM_JOB_ID}.batch" --format=JobID,MaxRSS,MaxVMSize,MaxDiskRead,MaxDiskWrite >>"$LOG_TIMES"
+echo "=============== Nodes Assigned =================" >>"$LOG_TIMES"
+scontrol show hostname $SLURM_NODELIST >>"$LOG_TIMES"
+echo >>"$LOG_TIMES"
+
+# ***********Exit************ #
+exit 0
---
+++ serial/src/multiply_matrix_blas.c
@@ -0,0 +1,138 @@
+/*****************************************************/
+/*    This program multiplies two square matrices    */
+/*          with static matrix allocation            */
+/*         reading from a binary input file          */
+/*****************************************************/
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <cblas.h>
+
+#ifndef SIZE
+#define SIZE 1024
+#endif
+#ifndef MAX_SIZE
+#define MAX_SIZE 1024
+#endif
+#ifndef NRUNS
+#define NRUNS 30
+#endif
+#ifndef MATRIX_TYPE
+#define MATRIX_TYPE int
+#endif
+
+MATRIX_TYPE A[SIZE][SIZE];
+MATRIX_TYPE B[SIZE][SIZE];
+MATRIX_TYPE C[SIZE][SIZE];
+
+double MULT_TIMES[NRUNS];
+struct timeval start, end;
+
+double total_time(struct timeval start, struct timeval end)
+{
+   return (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1e6;
+}
+
+double read_matrix(FILE *file, MATRIX_TYPE arr[SIZE][SIZE], int n, size_t start_element)
+{
+   gettimeofday(&start, NULL);
+
+   size_t offset = start_element * sizeof(MATRIX_TYPE);
+   fseek(file, offset, SEEK_SET);
+   fread(arr[0], sizeof(MATRIX_TYPE), n * n, file);
+
+   gettimeofday(&end, NULL);
+   return total_time(start, end);
+}
+
+double print_matrix(FILE *file, MATRIX_TYPE C[SIZE][SIZE], int n)
+{
+   gettimeofday(&start, NULL);
+
+   const char *format = (sizeof(MATRIX_TYPE) == sizeof(int)) ? "%d " : "%.2f ";
+
+   for (int i = 0; i < n; i++)
+   {
+      for (int j = 0; j < n; j++)
+      {
+         fprintf(file, format, C[i][j]);
+      }
+      fprintf(file, "\n");
+   }
+
+   gettimeofday(&end, NULL);
+   return total_time(start, end);
+}
+
+double multiply_matrices(MATRIX_TYPE A[SIZE][SIZE], MATRIX_TYPE B[SIZE][SIZE], MATRIX_TYPE C[SIZE][SIZE], int n)
+{
+   gettimeofday(&start, NULL);
+
+   // todo implement using ints
+   if (sizeof(MATRIX_TYPE) == sizeof(double))
+   {
+      cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                  n, n, n,
+                  1.0,
+                  A[0], n,
+                  B[0], n,
+                  0.0,
+                  C[0], n);
+   }
+   else if (sizeof(MATRIX_TYPE) == sizeof(float))
+   {
+      cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                  n, n, n,
+                  1.0f,
+                  A[0], n,
+                  B[0], n,
+                  0.0f,
+                  C[0], n);
+   }
+
+   gettimeofday(&end, NULL);
+   return total_time(start, end);
+}
+
+int main(int argc, char *argv[])
+{
+   if (sizeof(MATRIX_TYPE) != sizeof(float) && sizeof(MATRIX_TYPE) != sizeof(double))
+   {
+      printf("Only float and double types are supported\n");
+      return 1;
+   }
+
+   const char *matrix_file_name = argv[1];
+   const char *time_log_name = argv[2];
+   const char *result_log_name = argv[3];
+
+   double read_time = 0.0, write_time = 0.0, avg_mult_time = 0.0;
+
+   FILE *matrix_file = fopen(matrix_file_name, "rb");
+   read_time = read_matrix(matrix_file, A, SIZE, 0) + read_matrix(matrix_file, B, SIZE, MAX_SIZE * MAX_SIZE); // Start reading B from the end of A
+   fclose(matrix_file);
+
+   for (int i = 0; i <= NRUNS; i++)
+   {
+      if (i > 0)
+         MULT_TIMES[i - 1] = multiply_matrices(A, B, C, SIZE);
+      else
+         multiply_matrices(A, B, C, SIZE); // Warm up
+   }
+
+   FILE *result_log = fopen(result_log_name, "a");
+   write_time = print_matrix(result_log, C, SIZE);
+   fclose(result_log);
+
+   // Calculate average times
+   for (int i = 0; i < NRUNS; i++)
+      avg_mult_time += MULT_TIMES[i];
+
+   avg_mult_time /= NRUNS;
+
+   FILE *time_log = fopen(time_log_name, "a");
+   fprintf(time_log, "Read time: %.8f seconds\nWrite time: %.8f seconds\nMultiplication time (avg): %.8f seconds\n", read_time, write_time, avg_mult_time);
+   fclose(time_log);
+
+   return 0;
+}
